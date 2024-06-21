3f89ee04bc6cd229a9df3f6d9fe0771522e2de2e
Camille Michel <cammichel@sfr.fr>
2024-06-21 19:27:32 +0200

    MAJ responsive accueil

--- src/index.js
+++ src/index.js
@@ -11,16 +11,16 @@ import Error from "./pages/error/error";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-      <Router>
-        <Header />
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/location/:id" element={<Location />} />
-            <Route path="*" element={<Error />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </React.StrictMode>,
-  )
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/location/:id" element={<Location />} />
+          <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  </React.StrictMode>,
+)
